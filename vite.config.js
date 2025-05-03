26d5026a3471eecb2e00e936315c7c232d3b4d43
saturosukuna <rajeshravi67796@gmail.com>
2025-05-03 22:59:34 +0530

    ..

--- vite.config.js
+++ vite.config.js
@@ -5,17 +5,19 @@ import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/MainProject/',
   plugins: [
     tailwindcss(),
     react()],
-    build: {
-      outDir: path.resolve(__dirname, 'backend/dist'), 
-      chunkSizeWarningLimit: 1600,// Output build to backend folder
-      emptyOutDir: true
-    },
-    server: {
-      proxy: {
-        '/api': 'http://localhost:5000' // Proxy API requests to Express
-      }
-    }
+    
 })
+ // build: {
+    //   outDir: path.resolve(__dirname, 'backend/dist'), 
+    //   chunkSizeWarningLimit: 1600,// Output build to backend folder
+    //   emptyOutDir: true
+    // },
+    // server: {
+    //   proxy: {
+    //     '/api': 'http://localhost:5000' // Proxy API requests to Express
+    //   }
+    // },
